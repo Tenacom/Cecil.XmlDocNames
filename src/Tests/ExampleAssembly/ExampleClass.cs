a62c2ac0b755aa60ea0d597e117e8fdddc23e961
Riccardo De Agostini <riccardo@tenacom.it>
2020-03-01 16:26:18 +0100

    Add example types for unit tests.

---
+++ src/Tests/ExampleAssembly/ExampleClass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ExampleAssembly
+{
+    public sealed class ExampleClass
+    {
+        public int Field;
+
+        public ExampleClass()
+        {
+            Field = default;
+        }
+
+        public int Property { get; set; }
+
+        public void Method(int a, ref string b, byte[] c) => b = a + c.ToString();
+
+        public void GenericMethod<T>(T a, ref T b, T[] c, out IEnumerable<T> d)
+        {
+            b = a;
+            d = c;
+        }
+    }
+}
