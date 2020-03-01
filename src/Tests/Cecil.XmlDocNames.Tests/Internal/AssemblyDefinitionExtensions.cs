7197853567039154d794426f5be50cec34a91869
Riccardo De Agostini <riccardo@tenacom.it>
2020-03-01 16:26:58 +0100

    Add unit tests for StringBuilderExtensions.

---
+++ src/Tests/Cecil.XmlDocNames.Tests/Internal/AssemblyDefinitionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+
+namespace Cecil.XmlDocNames.Tests.Internal
+{
+    static class AssemblyDefinitionExtensions
+    {
+        public static TypeDefinition GetTypeThatStartsWith(this AssemblyDefinition @this, string prefix)
+            => @this.Modules.SelectMany(m => m.GetTypes()).First(t => t.Name.StartsWith(prefix, StringComparison.Ordinal));
+    }
+}
---
+++ src/Tests/Cecil.XmlDocNames.Tests/StringBuilderExtensionsTest.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Text;
+using Cecil.XmlDocNames.Tests.Internal;
+using NUnit.Framework;
+
+#pragma warning disable IDE0058 // Expression value is never used - Assert.Throws<> returns the thrown exception.
+
+namespace Cecil.XmlDocNames.Tests
+{
+    [TestFixture]
+    public class StringBuilderExtensionsTest
+    {
+        [Test]
+        public void AppendXmlDocName_WithNullMember_ThrowsArgumentNullException()
+        {
+            var sb = new StringBuilder();
+            Assert.Throws<ArgumentNullException>(() => sb.AppendXmlDocName(null!));
+        }
+
+        [Test]
+        public void AppendXmlDocName_WithNonGenericType_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilder();
+            using var assembly = TestUtility.ReadExampleAssembly();
+            var type = assembly.GetTypeThatStartsWith("Example");
+            var result = sb.AppendXmlDocName(type).ToString();
+            Assert.AreEqual("T:ExampleAssembly.ExampleClass", result);
+        }
+
+        [Test]
+        public void AppendXmlDocName_WithGenericType_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilder();
+            using var assembly = TestUtility.ReadExampleAssembly();
+            var type = assembly.GetTypeThatStartsWith("Generic");
+            var result = sb.AppendXmlDocName(type).ToString();
+            Assert.AreEqual("T:ExampleAssembly.GenericClass`1", result);
+        }
+
+        [Test]
+        public void AppendXmlDocName_WithField_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilder();
+            using var assembly = TestUtility.ReadExampleAssembly();
+            var type = assembly.GetTypeThatStartsWith("Example");
+            var field = type.GetFieldThatStartsWith("F");
+
+            var result = sb.AppendXmlDocName(field).ToString();
+            Assert.AreEqual("F:ExampleAssembly.ExampleClass.Field", result);
+        }
+
+        [Test]
+        public void AppendXmlDocName_WithProperty_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilder();
+            using var assembly = TestUtility.ReadExampleAssembly();
+            var type = assembly.GetTypeThatStartsWith("Example");
+            var property = type.GetPropertyThatStartsWith("P");
+
+            var result = sb.AppendXmlDocName(property).ToString();
+            Assert.AreEqual("P:ExampleAssembly.ExampleClass.Property", result);
+        }
+
+        [Test]
+        public void AppendXmlDocName_WithConstructorOfNonGenericType_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilder();
+            using var assembly = TestUtility.ReadExampleAssembly();
+            var type = assembly.GetTypeThatStartsWith("Example");
+            var method = type.GetMethodThatStartsWith(".c");
+
+            var result = sb.AppendXmlDocName(method).ToString();
+            Assert.AreEqual("M:ExampleAssembly.ExampleClass.#ctor", result);
+        }
+
+        [Test]
+        public void AppendXmlDocName_WithConstructorOfGenericType_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilder();
+            using var assembly = TestUtility.ReadExampleAssembly();
+            var type = assembly.GetTypeThatStartsWith("Generic");
+            var method = type.GetMethodThatStartsWith(".c");
+
+            var result = sb.AppendXmlDocName(method).ToString();
+            Assert.AreEqual("M:ExampleAssembly.GenericClass`1.#ctor", result);
+        }
+
+        [Test]
+        public void AppendXmlDocName_WithNonGenericMethodOfNonGenericType_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilder();
+            using var assembly = TestUtility.ReadExampleAssembly();
+            var type = assembly.GetTypeThatStartsWith("Example");
+            var method = type.GetMethodThatStartsWith("M");
+
+            var result = sb.AppendXmlDocName(method).ToString();
+            Assert.AreEqual("M:ExampleAssembly.ExampleClass.Method(System.Int32,System.String@,System.Byte[])", result);
+        }
+
+        [Test]
+        public void AppendXmlDocName_WithGenericMethodOfNonGenericType_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilder();
+            using var assembly = TestUtility.ReadExampleAssembly();
+            var type = assembly.GetTypeThatStartsWith("Example");
+            var method = type.GetMethodThatStartsWith("G");
+
+            var result = sb.AppendXmlDocName(method).ToString();
+            Assert.AreEqual("M:ExampleAssembly.ExampleClass.GenericMethod``1(``0,``0@,``0[],System.Collections.Generic.IEnumerable{``0}@)", result);
+        }
+
+        [Test]
+        public void AppendXmlDocName_WithNonGenericMethodOfGenericType_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilder();
+            using var assembly = TestUtility.ReadExampleAssembly();
+            var type = assembly.GetTypeThatStartsWith("Generic");
+            var method = type.GetMethodThatStartsWith("M");
+
+            var result = sb.AppendXmlDocName(method).ToString();
+            Assert.AreEqual("M:ExampleAssembly.GenericClass`1.Method(System.Int32,System.String@,System.Byte[])", result);
+        }
+
+        [Test]
+        public void AppendXmlDocName_WithGenericMethodOfGenericType_ReturnsCorrectValue()
+        {
+            var sb = new StringBuilder();
+            using var assembly = TestUtility.ReadExampleAssembly();
+            var type = assembly.GetTypeThatStartsWith("Generic");
+            var method = type.GetMethodThatStartsWith("G");
+
+            var result = sb.AppendXmlDocName(method).ToString();
+            Assert.AreEqual("M:ExampleAssembly.GenericClass`1.GenericMethod``1(`0,`0@,``0[],System.Collections.Generic.IEnumerable{``0}@)", result);
+        }
+    }
+}
