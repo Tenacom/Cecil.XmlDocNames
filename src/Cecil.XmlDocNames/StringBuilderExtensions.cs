8ef537d60cd440ceae55090b0b80977193a644f5
Riccardo De Agostini <riccardo@tenacom.it>
2020-09-11 14:01:03 +0200

    Add file headers.

--- src/Cecil.XmlDocNames/StringBuilderExtensions.cs
+++ src/Cecil.XmlDocNames/StringBuilderExtensions.cs
@@ -1,4 +1,13 @@
-﻿using System.Text;
+﻿// -----------------------------------------------------------------------------------
+// Copyright (C) Tenacom. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+//
+// Part of this file may be third-party code, distributed under a compatible license.
+// See THIRD-PARTY-NOTICES file in the project root for third-party copyright notices.
+// -----------------------------------------------------------------------------------
+
+using System.Text;
 using JetBrains.Annotations;
 
 namespace Cecil.XmlDocNames
--- tests/Cecil.XmlDocNames.Tests/StringBuilderExtensionsTest.cs
+++ tests/Cecil.XmlDocNames.Tests/StringBuilderExtensionsTest.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿// -----------------------------------------------------------------------------------
+// Copyright (C) Tenacom. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+//
+// Part of this file may be third-party code, distributed under a compatible license.
+// See THIRD-PARTY-NOTICES file in the project root for third-party copyright notices.
+// -----------------------------------------------------------------------------------
+
+using System;
 using System.Text;
 using Cecil.XmlDocNames.Tests.Internal;
 using NUnit.Framework;
